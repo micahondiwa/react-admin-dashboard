432de9fc30f57cf62a18933f124c5ac658e86f7a
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-14 17:26:19 +0300

    themes

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,6 @@
-/*Getting all the colors and typography of the website*/
+// Getting all the colors and typography of the website
 
 import { createContext, useState, useMemo } from "react";
+import {createTheme } from "@mui/material/styles";
+
+// Color design token
