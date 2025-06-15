5b8e7aab575be5451ae51693488d7adc01ccfd74
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-15 18:57:51 +0300

    Theme settings

--- src/theme.js
+++ src/theme.js
@@ -194,3 +194,24 @@ export const themeSettings = (mode) => {
   };
 };
 
+
+// context for the color mode
+export const ColorModeContext = createContext ({
+  toggleColorMode: () => {} 
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState("dark");
+
+  const colorMode = useMode(
+    () => ({
+      toggleColorMode: () => 
+        useMode((prev) => (prev == "light" ? "dark" : "light"))
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return [theme, colorMode];
+}
