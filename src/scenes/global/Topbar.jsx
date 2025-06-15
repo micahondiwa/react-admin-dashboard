91c184c1968f566e17bf4ce01103c8aefd012e5e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-15 19:25:23 +0300

    Theme

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -13,8 +13,10 @@ const Topbar = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
-    
-    return <div>Topbar</div>
+
+    return (<Box display = "flex" justifyContent = "space-between" p={2}>
+        
+    </Box>)
 };
 
 export default Topbar;
