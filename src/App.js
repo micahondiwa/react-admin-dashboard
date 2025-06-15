85509453f0e407c0277197c787c9b89fc890d5be
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-15 19:08:17 +0300

    Main component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import {ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
-  return <div className='app'></div>
+  const [theme, colorMode] = useMode();
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <div className='app'>
+        <main className='content'></main>
+        </div>;
+    </ThemeProvider>
+  </ColorModeContext.Provider>
+
+  )
 }
 
 export default App;
