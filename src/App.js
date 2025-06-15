ebd317348c4a2f933087ac930f2ff9a1f1ee58da
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-15 19:02:19 +0300

    Appjs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import {ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
-function AppBar() {
+function App() {
   return <div className='app'></div>
 }
 
-export default AppBar;
+export default App;
